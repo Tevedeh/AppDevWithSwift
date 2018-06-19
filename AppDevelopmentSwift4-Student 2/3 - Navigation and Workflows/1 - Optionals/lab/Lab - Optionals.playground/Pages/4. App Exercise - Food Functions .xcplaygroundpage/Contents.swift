cdacc62cbaf9160e877f1187d1fa4c24342d57ee
Tristan Anderson <systeatristan@gmail.com>
2018-06-18 23:31:24 -0500

    Working slow

--- AppDevelopmentSwift4-Student 2/3 - Navigation and Workflows/1 - Optionals/lab/Lab - Optionals.playground/Pages/4. App Exercise - Food Functions .xcplaygroundpage/Contents.swift
+++ AppDevelopmentSwift4-Student 2/3 - Navigation and Workflows/1 - Optionals/lab/Lab - Optionals.playground/Pages/4. App Exercise - Food Functions .xcplaygroundpage/Contents.swift
@@ -29,6 +29,8 @@ print(mealFunc(meal: Breakfast))
  
  Write a function that will check to see if your meal log (a dictionary like that in the previous exercise) is saved to the device. If it is, return the meal log. If it isn't, return an empty dictionary of type `[String: Any]`. The code you should use in this exercise for retrieving something saved to the device is `UserDefaults.standard.dictionary(forKey: "mealLog")`. This code will return an optional `[String: Any]`. If it return a value, that is your meal log. If it returns `nil`, then no meal log has been saved. Call the function and print the return value.
  */
-
+func ifMealLog(){
+    if
+}
 
 //: [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Failable Initializers](@next)
